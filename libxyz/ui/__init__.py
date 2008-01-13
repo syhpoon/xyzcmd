fb873e6e0f409a03abd5cb81b1af3e2b5d2d5a77
Max E. Kuznecov <mek@mek.uz.ua>
2008-01-13 03:38:47 +0200

    Writing spec

---
+++ libxyz/ui/__init__.py
@@ -0,0 +1,15 @@
+"""\
+Module contains user interface widgets
+"""
+
+#
+# Max E. Kuznecov ~syhpoon <mek@mek.uz.ua> 2008
+#
+
+__all__ = (
+    'Menu',
+    'Button',
+)
+
+from menu import Menu
+from button import Button
