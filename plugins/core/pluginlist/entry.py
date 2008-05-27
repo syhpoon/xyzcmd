ddba965605c8a14c7c38c9eae0e52efd5c73a37e
Max E. Kuznecov <mek@mek.uz.ua>
2008-05-27 19:39:22 +0300

    Move PluginEntry to a new file

---
+++ plugins/core/pluginlist/entry.py
@@ -0,0 +1,71 @@
+#-*- coding: utf8 -*
+#
+# Max E. Kuznecov ~syhpoon <mek@mek.uz.ua> 2008
+#
+# This file is part of XYZCommander.
+# XYZCommander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# XYZCommander is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser Public License for more details.
+# You should have received a copy of the GNU Lesser Public License
+# along with XYZCommander. If not, see <http://www.gnu.org/licenses/>.
+
+class PluginEntry(lowui.FlowWidget):
+    """
+    Plugins list entry
+    """
+
+    def __init__(self, plugin, attr, enter_cb):
+        super(PluginEntry, self).__init__()
+
+        self.plugin = plugin
+        self.enter_cb = enter_cb
+
+        self._text = u"%s%*s (%s)"
+        self._attr = attr(uilib.XYZListBox.resolution, u"selected")
+        self._content = lowui.Text(self._text)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def selectable(self):
+        return True
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def rows(self, (maxcol,), focus=False):
+        return 1
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def render(self, (maxcol,), focus=False):
+        def percent(per, total):
+            return int((total / 100.0) * per)
+
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        _pad = percent(40, maxcol) - len(self.plugin.ns)
+
+        _text = self._text % (self.plugin.ns, _pad,
+                              self.plugin.VERSION,
+                              self.plugin.BRIEF_DESCRIPTION)
+        if focus:
+            self._content.set_text((self._attr, _text))
+        else:
+            self._content.set_text(_text)
+
+        return self._content.render((maxcol,), focus)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def keypress(self, (maxcol,), key):
+        if key == uilib.Keys.ENTER:
+            try:
+                self.enter_cb()
+            except TypeError:
+                pass
+        else:
+            return key
--- plugins/core/pluginlist/main.py
+++ plugins/core/pluginlist/main.py
@@ -14,7 +14,12 @@
 # You should have received a copy of the GNU Lesser Public License
 # along with XYZCommander. If not, see <http://www.gnu.org/licenses/>.
 
+import libxyz.ui as uilib
+
+from libxyz.ui import lowui
 from libxyz.core.plugins import BasePlugin
+
+from entry import PluginEntry
 
 class XYZPlugin(BasePlugin):
     """
@@ -25,7 +30,8 @@ class XYZPlugin(BasePlugin):
     AUTHOR = u"Max E. Kuznecov ~syhpoon <mek@mek.uz.ua>"
     VERSION = u"0.1"
     BRIEF_DESCRIPTION = u"Show plugin list"
-    FULL_DESCRIPTION = u""
+    FULL_DESCRIPTION = u"Show all currently loaded plugins and assosiated "\
+                       u"information"
     NAMESPACE = u"core"
 
     def __init__(self, xyz):
@@ -40,4 +46,26 @@ class XYZPlugin(BasePlugin):
         Show plugins list
         """
 
-        pass
+        _plugins = sorted(self.xyz.pm.get_all_loaded().values(),
+                          lambda x, y: cmp(x.ns, y.ns))
+
+        self._walker = lowui.SimpleListWalker([
+                           PluginEntry(_obj, self.xyz.skin.attr, self._info)
+                           for _obj in _plugins])
+
+        _list = uilib.XYZListBox(self.xyz, self.xyz.top, self._walker,
+                                 _(u"Active plugins list"))
+        _list.show()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _info(self):
+        """
+        Show plugin detailed info
+        """
+
+        _w = self._walker.get_focus()[0]
+        _plugin = _w.plugin
+
+        uilib.MessageBox(self.xyz, self.xyz.top, "%s" %
+          str([(k, v.__doc__) for k,v in _plugin.public.iteritems()])).show()
