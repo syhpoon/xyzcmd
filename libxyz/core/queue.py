d9d971f2d405a0b48868489dd65add9a5cb7cb6d
Max E. Kuznecov <mek@mek.uz.ua>
2008-06-01 23:58:30 +0300

    Queue structure - a limited size list

---
+++ libxyz/core/queue.py
@@ -0,0 +1,38 @@
+#-*- coding: utf8 -*
+#
+# Max E. Kuznecov ~syhpoon <mek@mek.uz.ua> 2008
+#
+# This file is part of XYZCommander.
+# XYZCommander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# XYZCommander is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser Public License for more details.
+# You should have received a copy of the GNU Lesser Public License
+# along with XYZCommander. If not, see <http://www.gnu.org/licenses/>.
+
+class Queue(list):
+    """
+    Fixed-sized list
+    """
+
+    def __init__(self, maxsize):
+        super(Queue, self).__init__()
+
+        self._maxsize = maxsize
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def push(self, item):
+        """
+        Push a new item to queue. If queue already contains maxsize elements
+        replace the oldest one.
+        """
+
+        if len(self) == self._maxsize:
+            del(self[0])
+
+        self.append(item)
