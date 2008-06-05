bc3b74c796d85986d6dbfbb2ea063bb58bfa530e
Max E. Kuznecov <mek@mek.uz.ua>
2008-06-06 00:26:43 +0300

    New widget - divider line with title

---
+++ libxyz/ui/separator.py
@@ -0,0 +1,86 @@
+#-*- coding: utf8 -*
+#
+# Max E. Kuznecov ~syhpoon <mek@mek.uz.ua> 2008
+#
+# This file is part of XYZCommander.
+# XYZCommander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# XYZCommander is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser Public License for more details.
+# You should have received a copy of the GNU Lesser Public License
+# along with XYZCommander. If not, see <http://www.gnu.org/licenses/>.
+
+from libxyz.ui import lowui
+
+class Separator(lowui.FlowWidget):
+    """
+    Horizontal separator widget
+    """
+
+    ignore_focus = True
+
+    def __init__(self, title, div_char=None, title_attr=None, div_attr=None,
+                 align=None, top=0, bottom=0):
+        """
+        @param title: Title
+        @param div_char: Character to repeat across line
+        @param title_attr: Attribute of title text
+        @param div_attr: Attribute of divider line
+        @param align: TODO:
+        @param top: number of blank lines above
+        @param bottom: number of blank lines below
+        """
+
+        super(Separator, self).__init__()
+
+        _title = " %s " % title
+
+        self.title_len = len(_title)
+        # TODO: align
+        self.align = align
+        self.div_char = lowui.escape.utf8decode("─")
+        self.div_attr = div_attr
+        self.top = top
+        self.bottom = bottom
+
+        if title_attr is not None:
+            self.text = lowui.Text((title_attr, _title))
+        else:
+            self.text = lowui.Text(_title)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def rows(self, (maxcol,), focus=False):
+        """
+        Return the number of lines that will be rendered.
+        """
+
+        return self.top + 1 + self.bottom
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def render(self, (maxcol,), focus=False):
+        """
+        Render the separator as canvas and return it.
+        """
+
+        sep_len = (maxcol - self.title_len) / 2
+
+        canv_begin = lowui.Text((self.div_attr, self.div_char * sep_len))
+        canv_begin = canv_begin.render((maxcol,))
+        canv_text = self.text.render((maxcol,))
+        canv_end = lowui.Text((self.div_attr, self.div_char * sep_len))
+        canv_end = canv_end.render((maxcol,))
+
+        canv = lowui.CanvasJoin([(canv_begin, None, False, sep_len),
+                                 (canv_text, None, False, self.title_len),
+                                 (canv_end, None, False, sep_len)])
+
+        if self.top or self.bottom:
+            canv.pad_trim_top_bottom(self.top, self.bottom)
+
+        return canv
