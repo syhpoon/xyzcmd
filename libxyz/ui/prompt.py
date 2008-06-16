1c0a94973508678ce88966885c2948b82d668a44
Max E. Kuznecov <mek@mek.uz.ua>
2008-06-17 00:22:05 +0300

    Prompt widget

---
+++ libxyz/ui/prompt.py
@@ -0,0 +1,43 @@
+#-*- coding: utf8 -*
+#
+# Max E. Kuznecov ~syhpoon <mek@mek.uz.ua> 2008
+#
+# This file is part of XYZCommander.
+# XYZCommander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# XYZCommander is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser Public License for more details.
+# You should have received a copy of the GNU Lesser Public License
+# along with XYZCommander. If not, see <http://www.gnu.org/licenses/>.
+
+from libxyz.ui import lowui
+
+class Prompt(lowui.Text):
+    """
+    Command line prompt
+    """
+
+    def __init__(self, text, attr=None):
+        """
+        @param text: Prompt text
+        """
+
+        if attr is not None:
+            _arg = (attr, text)
+        else:
+            _arg = text
+
+        super(Prompt, self).__init__(_arg)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def length(self):
+        """
+        Return prompt text length
+        """
+
+        return len(self.get_text()[0])
