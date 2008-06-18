dcc2384c24e377e658046b5507269abe7d99d71f
Max E. Kuznecov <mek@mek.uz.ua>
2008-06-18 12:42:26 +0300

    UI plugins dir

---
+++ plugins/ui/testinput/main.py
@@ -0,0 +1,57 @@
+#-*- coding: utf8 -*
+#
+# Max E. Kuznecov <mek@mek.uz.ua> 2008
+#
+
+from libxyz.core.plugins import BasePlugin
+
+class XYZPlugin(BasePlugin):
+    "Plugin testinput"
+
+    # Plugin name
+    NAME = u"testinput"
+
+    # AUTHOR: Author name
+    AUTHOR = u"Max E. Kuznecov <mek@mek.uz.ua>"
+
+    # VERSION: Plugin version
+    VERSION = u"0.1"
+
+    # Brief one line description
+    BRIEF_DESCRIPTION = u""
+
+    # Full plugin description
+    FULL_DESCRIPTION = u""
+
+    # NAMESPACE: Plugin namespace. For detailed information about
+    #            namespaces see Plugins chapter of XYZCommander user manual.
+    #            Full namespace path to method is:
+    #            xyz:plugins:misc:hello:SayHello
+
+    NAMESPACE = u"ui"
+
+    # MIN_XYZ_VERSION: Minimal XYZCommander version
+    #                  the plugin is compatible with
+    MIN_XYZ_VERSION = None
+
+    # Plugin documentation
+    DOC = None
+
+    # Plugin home-page
+    HOMEPAGE = None
+
+    def __init__(self, xyz):
+        super(XYZPlugin, self).__init__(xyz)
+
+        self.public = {}
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def prepare(self):
+        pass
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def finalize(self):
+        pass
+
