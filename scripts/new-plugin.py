039792f8dd63bcacb94c1ef5373d87c9492330c0
Max E. Kuznecov <syhpoon@syhpoon.name>
2008-08-05 22:26:57 +0300

    Remove self.public from plugin template as public method must be exported using export()

--- scripts/new-plugin.py
+++ scripts/new-plugin.py
@@ -71,8 +71,6 @@ class XYZPlugin(BasePlugin):
     def __init__(self, xyz):
         super(XYZPlugin, self).__init__(xyz)
 
-        self.public = {}
-
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def prepare(self):
