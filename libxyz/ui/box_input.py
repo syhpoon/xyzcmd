5d17321d4da4eb674ade9c49569da44399866f58
Max E. Kuznecov <syhpoon@syhpoon.name>
2008-09-01 00:19:11 +0300

    New widget - input box

---
+++ libxyz/ui/box_input.py
@@ -0,0 +1,164 @@
+#-*- coding: utf8 -*
+#
+# Max E. Kuznecov ~syhpoon <syhpoon@syhpoon.name> 2008
+#
+# This file is part of XYZCommander.
+# XYZCommander is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# XYZCommander is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser Public License for more details.
+# You should have received a copy of the GNU Lesser Public License
+# along with XYZCommander. If not, see <http://www.gnu.org/licenses/>.
+
+from libxyz.ui import lowui
+from libxyz.ui import align
+from libxyz.ui import Box
+from libxyz.ui import Border
+
+import libxyz.ui
+
+class InputBox(Box):
+    """
+    Shows a message and waits for input
+    """
+
+    # Skin rulesets resolution order
+    resolution = (u"input_box", u"box", u"widget")
+
+    def __init__(self, xyz, body, message, title="", width=60):
+        """
+        @param xyz: XYZ dictionary
+        @param body: Top-level widget
+        @param message: Message to display
+        @param title: Box title
+        @param width: Box width (including mount box)
+
+        Required resources: title, box, border, mount, button
+        """
+
+        super(InputBox, self).__init__(xyz, body, message, title, width)
+        self.calc_size(6)
+
+        self.keys = libxyz.ui.Keys()
+
+        self._ok_txt = _(u"OK")
+        self._cancel_txt = _(u"Cancel")
+        self._value = False
+
+        self._buttons = self._init_buttons()
+
+        _title = self._strip_title(title.replace(u"\n", u" "))
+
+        if _title:
+            _title = (_title, self._attr(u"title"))
+        else:
+            _title = None
+
+        _mount = lowui.AttrWrap(lowui.Filler(lowui.Text(u"")),
+                                self._attr(u"mount"))
+
+        # Main dialog text
+        _text = lowui.Text((self._attr(u"box"), message), align.CENTER)
+        _blank = lowui.Text((self._attr(u"box"), ""))
+
+        _edit = lowui.Edit()
+
+        _widgets = [_text, _blank, _edit, _blank, self._buttons]
+        _box = lowui.Filler(lowui.Pile(_widgets), valign=align.BOTTOM)
+        _box = Border(_box, _title, self._attr(u"border"))
+        _box = lowui.AttrWrap(_box, self._attr(u"box"))
+
+        _mount = lowui.Overlay(_mount, body, align.CENTER, self.full_width,
+                             align.MIDDLE, self.full_height)
+        _box = lowui.Overlay(_box, _mount, align.CENTER, self.box_width,
+                             align.MIDDLE, self.box_height)
+
+        self.parent_init(_box)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def show(self, dim=None):
+        """
+        Show box and return pressed button.
+        True if YES pressed, False if NO
+        """
+
+        if dim is None:
+            dim = self.screen.get_cols_rows()
+        while True:
+            try:
+                self.screen.draw_screen(dim, self.render(dim, True))
+
+                _keys = self.xyz.input.get()
+
+                if [x for x in (self.keys.LEFT,
+                                self.keys.RIGHT,
+                                self.keys.UP,
+                                self.keys.DOWN,
+                                ) if x in _keys]:
+                    self._change_focus(_keys)
+
+                if self.keys.ESCAPE in _keys:
+                    return False
+
+                if self.keys.ENTER in _keys:
+                    _button = self._buttons.focus_cell.get_w()
+                    self._pressed(_button)
+                    return self._value
+            except KeyboardInterrupt:
+                continue
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _init_buttons(self):
+        _ok_len = len(self._ok_txt)
+        _cancel_len = len(self._cancel_txt)
+        _b_attr = self._attr("button")
+        _b_size = max(_ok_len, _cancel_len) + 4 # [ ... ]
+
+        self._b_ok = lowui.AttrWrap(libxyz.ui.XYZButton(self._ok_txt),_b_attr)
+        self._b_cancel = lowui.AttrWrap(libxyz.ui.XYZButton(self._cancel_txt),
+                                        _b_attr)
+
+        return lowui.GridFlow([self._b_ok, self._b_cancel], _b_size, 2, 0,
+                              align.CENTER)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _change_focus(self, keys):
+        """
+        Move focus
+        """
+
+        for key in keys:
+            _widget = None
+
+            # Move right
+            if key in (self.keys.RIGHT, self.keys.UP):
+                _widget = 1 # index
+            # Move left
+            elif key in (self.keys.LEFT, self.keys.DOWN):
+                _widget = 0
+            else:
+                pass
+
+            if _widget is not None:
+                self._buttons.set_focus(_widget)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _pressed(self, button):
+        """
+        Button pressed
+        """
+
+        _label = button.get_label()
+
+        if _label == self._ok_txt:
+            self._value = True
+        else:
+            self._value = False
