4981b80a336b5a1f74d99a0694e6cc94b158989f
Max E. Kuznecov <syhpoon@syhpoon.name>
2009-08-15 03:54:46 +0300

    Update MM

--- doc/mm/xyzcmd.mm
+++ doc/mm/xyzcmd.mm
@@ -5,24 +5,24 @@
 <node CREATED="1244495489656" ID="_" MODIFIED="1244501325177" POSITION="right" TEXT="UI">
 <font NAME="Tahoma" SIZE="12"/>
 </node>
-<node CREATED="1244495492427" ID="Freemind_Link_513771550" MODIFIED="1249759982633" POSITION="left" TEXT="VFS">
+<node CREATED="1244495492427" FOLDED="true" ID="Freemind_Link_513771550" MODIFIED="1249759982633" POSITION="left" TEXT="VFS">
 <edge COLOR="#cc0033"/>
 <font NAME="Tahoma" SIZE="12"/>
-<node CREATED="1249759904224" ID="Freemind_Link_71273883" MODIFIED="1249760613279" TEXT="Path-based dispatcher - VFSDispatcher">
-<node CREATED="1249759934924" ID="Freemind_Link_1542378353" MODIFIED="1249759947713" TEXT="TarVFS">
+<node CREATED="1249759904224" FOLDED="true" ID="Freemind_Link_71273883" MODIFIED="1249760613279" TEXT="Path-based dispatcher - VFSDispatcher">
+<node CREATED="1249759934924" FOLDED="true" ID="Freemind_Link_1542378353" MODIFIED="1249759947713" TEXT="TarVFS">
 <node CREATED="1249760006326" ID="Freemind_Link_1058723409" MODIFIED="1249768114520" TEXT="tar:/path/to/tar/file.tar#vfs#/path/inside/archive"/>
 </node>
-<node CREATED="1249759956848" ID="Freemind_Link_943679513" MODIFIED="1249759968268" TEXT="ZipVFS">
+<node CREATED="1249759956848" FOLDED="true" ID="Freemind_Link_943679513" MODIFIED="1249759968268" TEXT="ZipVFS">
 <node CREATED="1249760126331" ID="Freemind_Link_1188513307" MODIFIED="1249760782607" TEXT="zip:/path/to/zip/file.zip#/file"/>
 </node>
-<node CREATED="1249761202625" ID="Freemind_Link_828272914" MODIFIED="1249761210178" TEXT="Nested VFS objects">
+<node CREATED="1249761202625" FOLDED="true" ID="Freemind_Link_828272914" MODIFIED="1249761210178" TEXT="Nested VFS objects">
 <node CREATED="1249761211444" ID="Freemind_Link_1672617694" MODIFIED="1249768132579" TEXT="tar:/zip:/tar:/tmp/file1.tar#vfs#/file2.zip#vfs#/file-inside-zip.tar#vfs#file-inside-tar-inside-zip"/>
 </node>
 </node>
 </node>
-<node CREATED="1244495503893" ID="Freemind_Link_1083954312" MODIFIED="1244501325175" POSITION="right" TEXT="Keys">
+<node CREATED="1244495503893" FOLDED="true" ID="Freemind_Link_1083954312" MODIFIED="1244501325175" POSITION="right" TEXT="Keys">
 <font NAME="Tahoma" SIZE="12"/>
-<node CREATED="1247259950980" ID="Freemind_Link_778082928" MODIFIED="1247259955123" TEXT="Shortcut">
+<node CREATED="1247259950980" FOLDED="true" ID="Freemind_Link_778082928" MODIFIED="1247259955123" TEXT="Shortcut">
 <node CREATED="1247259974933" ID="Freemind_Link_1038917157" MODIFIED="1247259989566" TEXT="raw"/>
 </node>
 </node>
@@ -32,7 +32,7 @@
 <node CREATED="1244495510007" ID="Freemind_Link_522268654" MODIFIED="1244501325173" POSITION="right" TEXT="Skins">
 <font NAME="Tahoma" SIZE="12"/>
 </node>
-<node CREATED="1244495526482" ID="Freemind_Link_1653593100" MODIFIED="1244501325171" POSITION="left" TEXT="Parsers">
+<node CREATED="1244495526482" FOLDED="true" ID="Freemind_Link_1653593100" MODIFIED="1244501325171" POSITION="left" TEXT="Parsers">
 <edge COLOR="#0000cc"/>
 <font NAME="Tahoma" SIZE="12"/>
 <node CREATED="1244495535504" ID="Freemind_Link_1432536488" MODIFIED="1244501325171" TEXT="Block">
@@ -47,21 +47,17 @@
 <node CREATED="1244495544485" ID="Freemind_Link_1436796589" MODIFIED="1244501325167" TEXT="Regexp">
 <font NAME="Tahoma" SIZE="12"/>
 </node>
-<node CREATED="1244500228761" ID="Freemind_Link_1274127063" MODIFIED="1244501325166" TEXT="LR">
+<node CREATED="1244500228761" FOLDED="true" ID="Freemind_Link_1274127063" MODIFIED="1244501325166" TEXT="LR">
 <font NAME="Tahoma" SIZE="12"/>
 <node CREATED="1244500237630" ID="Freemind_Link_863082863" MODIFIED="1244501325164" TEXT="FS-Rules">
 <font NAME="Tahoma" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1245361428409" ID="Freemind_Link_940491710" MODIFIED="1245361436843" TEXT="Implementation">
-<node CREATED="1245361507096" ID="Freemind_Link_981733442" MODIFIED="1245361539882" TEXT="XYZObject singleton"/>
-<node CREATED="1245361641193" ID="Freemind_Link_1911204732" MODIFIED="1245361645041" TEXT="XYZShortcut"/>
-</node>
-<node CREATED="1244500242900" ID="Freemind_Link_1799785524" MODIFIED="1250201159743" TEXT="DSL">
+<node CREATED="1244500242900" FOLDED="true" ID="Freemind_Link_1799785524" MODIFIED="1250201159743" TEXT="DSL">
 <edge COLOR="#000066"/>
 <cloud/>
 <font NAME="Tahoma" SIZE="12"/>
-<node CREATED="1244501362238" ID="Freemind_Link_1837826538" MODIFIED="1244501539906" TEXT="Purposes">
+<node CREATED="1244501362238" FOLDED="true" ID="Freemind_Link_1837826538" MODIFIED="1244501539906" TEXT="Purposes">
 <node CREATED="1244500368153" ID="Freemind_Link_588763910" MODIFIED="1244501325161" STYLE="bubble" TEXT="Set variables">
 <font NAME="Tahoma" SIZE="12"/>
 </node>
@@ -86,63 +82,63 @@
 </node>
 <node CREATED="1244571892922" ID="Freemind_Link_255526285" MODIFIED="1244571899817" TEXT="Plugins configuration"/>
 </node>
-<node CREATED="1245361398689" ID="Freemind_Link_855729037" MODIFIED="1245361403285" TEXT="interface">
-<node CREATED="1245359440779" ID="Freemind_Link_1142229797" MODIFIED="1245368124565" TEXT="kbd(*keys)">
-<node CREATED="1245360061472" ID="Freemind_Link_672485290" MODIFIED="1245360075054" TEXT="Create shortcut as a key sequence">
+<node CREATED="1245361398689" FOLDED="true" ID="Freemind_Link_855729037" MODIFIED="1245361403285" TEXT="interface">
+<node CREATED="1245359440779" FOLDED="true" ID="Freemind_Link_1142229797" MODIFIED="1245368124565" TEXT="kbd(*keys)">
+<node CREATED="1245360061472" FOLDED="true" ID="Freemind_Link_672485290" MODIFIED="1245360075054" TEXT="Create shortcut as a key sequence">
 <node CREATED="1244662573839" ID="Freemind_Link_310059790" MODIFIED="1245585475712" TEXT="kbd(&quot;UP&quot;)&#xa;kbd(&quot;META-s&quot;, &quot;x&quot;)&#xa;kbd(&quot;CTRL-z&quot;)&#xa;kbd(&quot;F5&quot;)">
 <icon BUILTIN="xmag"/>
 </node>
 </node>
 </node>
-<node CREATED="1245357873731" ID="Freemind_Link_1403521052" MODIFIED="1245456321892" TEXT="let(varname, value, section=&quot;local&quot;)">
+<node CREATED="1245357873731" FOLDED="true" ID="Freemind_Link_1403521052" MODIFIED="1245456321892" TEXT="let(varname, value, section=&quot;local&quot;)">
 <icon BUILTIN="button_ok"/>
-<node CREATED="1245357962726" ID="Freemind_Link_275929665" MODIFIED="1245459340607" TEXT="Set variable.&#xa;Variable will be available in xyz.conf[section][varname]&#xa;If section is not provided - &quot;local&quot; will be used">
+<node CREATED="1245357962726" FOLDED="true" ID="Freemind_Link_275929665" MODIFIED="1245459340607" TEXT="Set variable.&#xa;Variable will be available in xyz.conf[section][varname]&#xa;If section is not provided - &quot;local&quot; will be used">
 <node COLOR="#000000" CREATED="1244500518886" ID="Freemind_Link_70663728" MODIFIED="1245459281297" TEXT="let(&quot;myvar&quot;, &quot;some value&quot;) # xyz.conf[&quot;local&quot;][&quot;myvar&quot;] == &quot;some value&quot;&#xa;***&#xa;let(&quot;skin&quot;, &quot;blue&quot;, section=&quot;xyz&quot;)">
 <font NAME="Tahoma" SIZE="12"/>
 <icon BUILTIN="xmag"/>
 </node>
 </node>
 </node>
-<node CREATED="1245583541470" ID="Freemind_Link_502370608" MODIFIED="1245586759557" TEXT="unlet(var, sec=&quot;local&quot;)">
+<node CREATED="1245583541470" FOLDED="true" ID="Freemind_Link_502370608" MODIFIED="1245586759557" TEXT="unlet(var, sec=&quot;local&quot;)">
 <icon BUILTIN="button_ok"/>
 <node CREATED="1245583554322" ID="Freemind_Link_1904306065" MODIFIED="1245583559521" TEXT="Unset variable"/>
 </node>
-<node CREATED="1245358523711" ID="Freemind_Link_1001198349" MODIFIED="1245701446418" TEXT="action(rule, function)">
+<node CREATED="1245358523711" FOLDED="true" ID="Freemind_Link_1001198349" MODIFIED="1245701446418" TEXT="action(rule, function)">
 <icon BUILTIN="button_ok"/>
-<node CREATED="1245358566594" ID="Freemind_Link_241216621" MODIFIED="1245358580145" TEXT="Set action based on rule to some function">
+<node CREATED="1245358566594" FOLDED="true" ID="Freemind_Link_241216621" MODIFIED="1245358580145" TEXT="Set action based on rule to some function">
 <node CREATED="1244663231419" ID="Freemind_Link_1449275902" MODIFIED="1245459373257" TEXT="action(r&quot;type{dir} or (link_type{dir} and link_exists{?})&quot;, lambda: call(&quot;:sys:panel:chdir&quot;, &quot;%O&quot;))">
 <icon BUILTIN="xmag"/>
 </node>
 </node>
 </node>
-<node CREATED="1245358854608" ID="Freemind_Link_171194029" MODIFIED="1245358878285" TEXT="set_prefix(shortcut)">
-<node CREATED="1245358880735" ID="Freemind_Link_1028978107" MODIFIED="1245358913602" TEXT="Set new prefix key">
+<node CREATED="1245358854608" FOLDED="true" ID="Freemind_Link_171194029" MODIFIED="1245358878285" TEXT="set_prefix(shortcut)">
+<node CREATED="1245358880735" FOLDED="true" ID="Freemind_Link_1028978107" MODIFIED="1245358913602" TEXT="Set new prefix key">
 <node CREATED="1244663685258" ID="Freemind_Link_1955451308" MODIFIED="1245358996993" TEXT="XYZ.set_prefix(XYZ.Shortcut(&quot;CTRL x&quot;))">
 <icon BUILTIN="xmag"/>
 </node>
 </node>
 </node>
-<node CREATED="1245359317854" ID="Freemind_Link_1693687529" MODIFIED="1245456326217" TEXT="load(plugin)">
+<node CREATED="1245359317854" FOLDED="true" ID="Freemind_Link_1693687529" MODIFIED="1245456326217" TEXT="load(plugin)">
 <icon BUILTIN="button_ok"/>
-<node CREATED="1245359338973" ID="Freemind_Link_472092876" MODIFIED="1245359352695" TEXT="Load method[s] from plugin">
+<node CREATED="1245359338973" FOLDED="true" ID="Freemind_Link_472092876" MODIFIED="1245359352695" TEXT="Load method[s] from plugin">
 <node COLOR="#000000" CREATED="1244500656760" ID="Freemind_Link_1615115653" MODIFIED="1245359369649" TEXT="XYZ.load(&quot;sys:panel:*&quot;)">
 <font NAME="Tahoma" SIZE="12"/>
 <icon BUILTIN="xmag"/>
 </node>
 </node>
 </node>
-<node CREATED="1245359395521" ID="Freemind_Link_1708893538" MODIFIED="1245586759550" TEXT="bind(method, shortcut, context=&quot;@&quot;)">
+<node CREATED="1245359395521" FOLDED="true" ID="Freemind_Link_1708893538" MODIFIED="1245586759550" TEXT="bind(method, shortcut, context=&quot;@&quot;)">
 <icon BUILTIN="button_ok"/>
-<node CREATED="1245359455981" ID="Freemind_Link_1257852318" MODIFIED="1245359465408" TEXT="Bind specified method to shortcut">
+<node CREATED="1245359455981" FOLDED="true" ID="Freemind_Link_1257852318" MODIFIED="1245359465408" TEXT="Bind specified method to shortcut">
 <node COLOR="#000000" CREATED="1244500735764" ID="Freemind_Link_1761463976" MODIFIED="1245459397322" TEXT="bind(&quot;:sys:panel:entry_next&quot;, shortcut(&quot;DOWN&quot;), context=&quot;@&quot;)">
 <font NAME="Tahoma" SIZE="12"/>
 <icon BUILTIN="xmag"/>
 </node>
 </node>
 </node>
-<node CREATED="1245359544399" ID="Freemind_Link_118928829" MODIFIED="1245703180982" TEXT="call(method, *args)">
+<node CREATED="1245359544399" FOLDED="true" ID="Freemind_Link_118928829" MODIFIED="1245703180982" TEXT="call(method, *args)">
 <icon BUILTIN="button_ok"/>
-<node CREATED="1245359576515" ID="Freemind_Link_1569658164" MODIFIED="1245359587464" TEXT="Call plugin method on args">
+<node CREATED="1245359576515" FOLDED="true" ID="Freemind_Link_1569658164" MODIFIED="1245359587464" TEXT="Call plugin method on args">
 <node CREATED="1245359588748" ID="Freemind_Link_1656389879" MODIFIED="1245359606267" TEXT="Substitutions">
 <icon BUILTIN="help"/>
 </node>
@@ -158,21 +154,22 @@
 <node CREATED="1245535477575" ID="Freemind_Link_1446723596" MODIFIED="1245703189796" TEXT="exec_file">
 <icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1245615450750" ID="Freemind_Link_1513951905" MODIFIED="1245703195556" TEXT="macro(macro_name)">
+<node CREATED="1245615450750" FOLDED="true" ID="Freemind_Link_1513951905" MODIFIED="1245703195556" TEXT="macro(macro_name)">
 <icon BUILTIN="button_ok"/>
-<node CREATED="1245615481016" ID="Freemind_Link_816910859" MODIFIED="1245615487833" TEXT="Expand macro">
+<node CREATED="1245615481016" FOLDED="true" ID="Freemind_Link_816910859" MODIFIED="1245615487833" TEXT="Expand macro">
 <node CREATED="1245615489117" ID="Freemind_Link_425587281" MODIFIED="1245615510487" TEXT="macro(&quot;CWD&quot;)"/>
 </node>
 </node>
 <node CREATED="1245874382917" ID="Freemind_Link_840364283" MODIFIED="1245874386689" TEXT="env()"/>
 <node CREATED="1245874433772" ID="Freemind_Link_463494108" MODIFIED="1245874435483" TEXT="shell"/>
-<node CREATED="1249762593698" ID="Freemind_Link_899153670" MODIFIED="1249762609900" TEXT="vfs(&quot;prefix&quot;, VFSObjectClass))">
-<node CREATED="1249762613211" ID="Freemind_Link_1369881740" MODIFIED="1249762869374" TEXT="Set VFS dispatcher handler for prefix">
+<node CREATED="1249762593698" FOLDED="true" ID="Freemind_Link_899153670" MODIFIED="1249762609900" TEXT="vfs(&quot;prefix&quot;, VFSObjectClass))">
+<node CREATED="1249762613211" FOLDED="true" ID="Freemind_Link_1369881740" MODIFIED="1249762869374" TEXT="Set VFS dispatcher handler for prefix">
 <node CREATED="1249762870789" ID="Freemind_Link_1944900231" MODIFIED="1249762903324" TEXT="vfs(&quot;tar&quot;, TarVFSObject)"/>
 </node>
 </node>
 </node>
 </node>
 </node>
+<node CREATED="1250201242815" ID="Freemind_Link_1290384240" MODIFIED="1250201244856" POSITION="right" TEXT="Hooks"/>
 </node>
 </map>
