6f0ba4fc872bfdceb47eef0f819b99a5e794fc09
Max E. Kuznecov <syhpoon@syhpoon.name>
2009-09-29 01:27:02 +0300

    Add an ability to remove tagged objects

--- plugins/vfs/vfsutils/main.py
+++ plugins/vfs/vfsutils/main.py
@@ -3,8 +3,6 @@
 # Max E. Kuznecov <syhpoon@syhpoon.name> 2009
 #
 
-import os
-
 import libxyz.ui as uilib
 
 from libxyz.core.utils import ustring
@@ -33,19 +31,9 @@ class XYZPlugin(BasePlugin):
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    def _load_panel(self):
+    def mkdir(self):
         """
-        Load :sys:panel plugin
-        """
-
-        if self._panel is None:
-            self._panel = self.xyz.pm.load(":sys:panel")
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    def mkdir(self, newdir=None):
-        """
-        Create new directory
+        Create new directory dialog
         """
 
         self._load_panel()
@@ -72,42 +60,67 @@ class XYZPlugin(BasePlugin):
 
     def remove(self):
         """
-        Remove VFS object
+        Remove VFS object dialog
         """
         
         self._load_panel()
 
-        selected = self._panel.get_selected()
+        tagged = self._panel.get_tagged()
 
-        # TODO: force setting
-        _deletep = uilib.YesNoBox(self.xyz, self.xyz.top,
-                                  _(u"Really remove %s (%s)?") % (
-                                      ustring(selected.name),
-                                      ustring(str(selected.ftype))),
+        if tagged:
+            objs, msg = tagged, _(u"Really remove %d objects?") % len(tagged)
+        else:
+            selected = self._panel.get_selected()
+            objs, msg = [selected], \
+                       _(u"Really remove %s (%s)?") % \
+                       (ustring(selected.name),
+                        ustring(selected.ftype))
+
+        _deletep = uilib.YesNoBox(self.xyz, self.xyz.top, msg,
                                   title=_(u"Remove object"))
 
         if not _deletep.show():
             return
 
-        if selected.is_dir() and not selected.is_dir_empty():
-            _rec = uilib.YesNoBox(
-                self.xyz, self.xyz.top,
-                _(u"Directory is not empty\nRemove it recursively?"),
-                title=_(u"Remove %s") % ustring(selected.full_path))
+        # TODO: All, None, Skip, Stop buttons
+        force = False
+        
+        for obj in objs:
+            if not force and obj.is_dir() and not obj.is_dir_empty():
+                _rec = uilib.YesNoBox(
+                    self.xyz, self.xyz.top,
+                    _(u"Directory is not empty\nRemove it recursively?"),
+                    title=_(u"Remove %s") % ustring(obj.full_path))
 
-            if not _rec.show():
-                return
+                if not _rec.show():
+                    continue
+                else:
+                    force = True
+                    
+            uilib.MessageBox(self.xyz, self.xyz.top,
+                             _(u"Removing object: %s") %
+                             ustring(obj.full_path),
+                             _(u"Removing")).show(wait=False)
 
-        uilib.MessageBox(self.xyz, self.xyz.top, _(u"Removing objects"),
-                         _(u"Removing")).show(wait=False)
+            try:
+                obj.remove()
+            except Exception, e:
+                uilib.ErrorBox(self.xyz, self.xyz.top,
+                               _(u"Unable to remove object: %s") %
+                               (ustring(str(e))),
+                               _(u"Error")).show()
+                xyzlog.error(_(u"Error removing object: %s") %
+                             ustring(str(e)))
+                break
 
-        try:
-            selected.remove()
-        except Exception, e:
-            uilib.ErrorBox(self.xyz, self.xyz.top,
-                           _(u"Unable to remove %s\n%s") %
-                           (ustring(selected.full_path), ustring(str(e))),
-                           _(u"Error")).show()
-            xyzlog.error(_(u"Error removing object: %s") % ustring(str(e)))
-        finally:
-            self._panel.reload()
+        self._panel.reload()
+                
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    def _load_panel(self):
+        """
+        Load :sys:panel plugin
+        """
+
+        if self._panel is None:
+            self._panel = self.xyz.pm.load(":sys:panel")
