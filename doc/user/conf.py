5b1b3fdd527452bb9e040c1791ac734a14462629
Max E. Kuznecov <syhpoon@syhpoon.name>
2009-10-23 00:29:14 +0300

    Update doc version

--- doc/user/conf.py
+++ doc/user/conf.py
@@ -42,9 +42,9 @@ copyright = '2008, Max E. Kuznecov'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.0.1'
+version = '0.0.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.1'
+release = '0.0.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
