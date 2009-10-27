cfdb72cb4079b1f26d1aa38889acfea49efd07ac
Max E. Kuznecov <syhpoon@syhpoon.name>
2009-10-28 00:28:52 +0200

    Add an ability to create debian .deb package from setup script (2)

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 ./gendoc.sh && \
 mkdir doc/user-manual && \
 mv doc/user/.build/html/* doc/user-manual && \
- ./setup.py sdist --formats=bztar
+# ./setup.py sdist --formats=bztar
+ ./setup.py bdist_debian
