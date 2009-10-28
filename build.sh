2588fd722c471f4c372608da8c83c3d9bd05a882
Max E. Kuznecov <syhpoon@syhpoon.name>
2009-10-29 00:57:28 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 ./gendoc.sh && \
 mkdir doc/user-manual && \
 mv doc/user/.build/html/* doc/user-manual && \
-# ./setup.py sdist --formats=bztar
- ./setup.py bdist_debian
+./setup.py sdist --formats=bztar
+# ./setup.py bdist_debian
