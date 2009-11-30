cc496a150f73ad448bc90dfb11d72f67494782d8
Max E. Kuznecov <syhpoon@syhpoon.name>
2009-12-01 00:28:18 +0200

    Add missing macros

--- libxyz/core/dsl.py
+++ libxyz/core/dsl.py
@@ -99,28 +99,52 @@ class XYZ(object):
         cls._instance = cls
 
         cls._env = {"XYZ": cls}
-        cls._env.update(dict([(f, getattr(cls, f)) for f in cls.api]))
+        cls._env.update(dict([(ff, getattr(cls, ff)) for ff in cls.api]))
 
         # Init macros
-        cls.macros["ACT_PATH"] = lambda: cls.xyz.pm.from_load(":sys:panel",
-                                               "get_selected")().path
-        cls.macros["ACT_CWD"] = lambda: cls.xyz.pm.from_load(":sys:panel",
-                                                             "cwd")()
-        cls.macros["INACT_CWD"] = lambda: cls.xyz.pm.from_load(":sys:panel",
-                                                "cwd")(False)
-        cls.macros["ACT_TAGGED"] = lambda: [x.name for x in
-                                            cls.xyz.pm.from_load(
-                                                ":sys:panel",
-                                                "get_tagged")()]
-        cls.macros["INACT_TAGGED"] = lambda: [x.name for x in
-                                              cls.xyz.pm.from_load(
-                                                  ":sys:panel",
-                                                  "get_tagged")(False)]
-
+        cls.macros = {}
+        cls.init_macros()
         return cls
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    @classmethod
+    def init_macros(cls):
+        cls.macros["ACT_CWD"] = lambda: cls.xyz.pm.from_load(":sys:panel",
+                                                             "cwd")()
+        cls.macros["INACT_CWD"] = lambda: cls.xyz.pm.from_load(":sys:panel",
+                                                               "cwd")(False)
+
+        cls.macros["INACT_PATH"] = lambda: \
+                                   cls.xyz.pm.from_load(":sys:panel",
+                                                        "get_selected"
+                                                        )(False).path
+        cls.macros["ACT_BASE"] = lambda: \
+                                 os.path.dirname(cls.macros["ACT_CWD"]())
+
+        cls.macros["INACT_BASE"] = lambda: \
+                                   os.path.dirname(cls.macros["INACT_CWD"]())
+
+        cls.macros["ACT_TAGGED"] = lambda: [x.full_path for x in
+                                            cls.xyz.pm.from_load(
+                                                ":sys:panel",
+                                                "get_tagged")()]
+        cls.macros["INACT_TAGGED"] = lambda: [x.full_path for x in
+                                              cls.xyz.pm.from_load(
+                                                  ":sys:panel",
+                                                  "get_tagged")(False)]
+
+        cls.macros["ACT_UNTAGGED"] = lambda: [x.full_path for x in
+                                              cls.xyz.pm.from_load(
+                                                  ":sys:panel",
+                                                  "get_untagged")()]
+        cls.macros["INACT_UNTAGGED"] = lambda: [x.full_path for x in
+                                                cls.xyz.pm.from_load(
+                                                    ":sys:panel",
+                                                    "get_untagged")(False)]
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
     @classmethod
     def _clear(cls):
         cls._instance = None
