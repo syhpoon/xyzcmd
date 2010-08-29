60be4440f9d3e24b15f53e88246418d76a61860a
Max E. Kuznecov <mek@mek.uz.ua>
2010-08-30 00:10:54 +0300

    Fix block parser error

--- libxyz/parser/block.py
+++ libxyz/parser/block.py
@@ -225,7 +225,7 @@ class BlockParser(BaseParser):
                     self._varname = self.var_transform(word)
                 except XYZValueError, e:
                     self.error(_(u"Variable transformation error: %s") %
-                               libxyz.core.utils.unicode(e))
+                               unicode(e))
             else:
                 self._varname = word
 
