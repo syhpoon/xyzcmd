1d1eb6ee660068b5acbc4d7c2da058c4ab488872
Max E. Kuznecov <mek@mek.uz.ua>
2010-09-15 00:32:14 +0300

    Update debian files

--- build_deb.sh
+++ build_deb.sh
@@ -2,7 +2,7 @@
 export DEBFULLNAME="Max E. Kuznecov"
 export DEBEMAIL="syhpoon@syhpoon.name"
 
-./build.sh && dpkg-buildpackage -uc
+./build.sh && dpkg-buildpackage -uc -S k4778E7C0
 
 # For ubuntu  PPA
 #./build.sh && dpkg-buildpackage -uc -S -k4778E7C0
